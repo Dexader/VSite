56b7b31f960784a4171b1290f9d063916955bc1d
Evgeniy Savin <82032696+Dexader@users.noreply.github.com>
2024-04-28 20:21:05 +0700

    Add Burger

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const menuBtn = document.querySelector('.burger-icon');
+const burger = document.querySelector('.burger-menu');
+
+menuBtn.addEventListener('mouseover', () => {
+	burger.classList.add('active');
+});
+
+menuBtn.addEventListener('mouseout', () => {
+	burger.classList.remove('active');
+});
+
 document.addEventListener('DOMContentLoaded', function() {
 	const emailInput = document.getElementById('emailInput');
 
