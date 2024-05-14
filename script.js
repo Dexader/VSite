8c7abcc453452deb1d348c4fa77839e5fcab28dc
Evgeniy Savin <82032696+Dexader@users.noreply.github.com>
2024-05-14 23:02:30 +0700

    Add Search

--- script.js
+++ script.js
@@ -36,13 +36,42 @@ const authPopup = document.querySelector('.auth-popup');
 const openBtnAuth = document.querySelector('.open-auth');
 const closeBtn = document.querySelector('.close-btn');
 const modalBackground = document.querySelector('.modal-background');
+const body = document.body;
 
 openBtnAuth.addEventListener('click', () => {
 	authPopup.style.display = 'block';
 	modalBackground.style.display = 'block';
+	body.style.height = '100vh';
+	body.style.overflowY = 'hidden';
 });
 
 closeBtn.addEventListener('click', () => {
 	authPopup.style.display = 'none';
 	modalBackground.style.display = 'none';
+	body.style.height = '';
+	body.style.overflowY = '';
+});
+
+//Search
+
+const searchIcon = document.querySelector('.search-icon');
+const searchInput = document.querySelector('.search-input');
+const searchContainer = document.querySelector('.search-container');
+const windowShadow = document.querySelector('.shadow');
+
+searchIcon.addEventListener('click', () => {
+	searchContainer.style.display = 'block';
+	windowShadow.style.display = 'block';
+	body.style.height = '100vh';
+	body.style.overflowY = 'hidden';
+});
+
+searchInput.addEventListener('keydown', event => {
+	if (event.code === 'Enter') {
+		searchInput.value = '';
+		searchContainer.style.display = 'none';
+		windowShadow.style.display = 'none';
+		body.style.height = '';
+		body.style.overflowY = '';
+	}
 });
